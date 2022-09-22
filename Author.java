ea9efb9b95ab1f90db5e95fd5b524d758c9b25c2
EarnestMD <85481264+EarnestMD@users.noreply.github.com>
2022-09-22 22:50:19 +0530

    Create Author.java

---
+++ Author.java
@@ -0,0 +1,16 @@
+package aggregation;
+
+public class Author{
+
+  String authorName;
+  int authorAge;
+
+  public Author(String AuthorName,int AuthorAge){
+    this.authorName=AuthorName;
+    this.authorAge=AuthorAge;
+   
+  }
+
+
+    
+}
